6ae72486fc4c0368e374f257f8e004067419bb74
Aksh <akshith.hiremath@gmail.com>
2020-10-22 22:49:56 +0530

    added a sort by date and alert

--- src/functions.js
+++ src/functions.js
@@ -1,42 +1,30 @@
 const fs = require('fs')
+const PATH_ = "E:/Coding Projects/desktopApps/electronApps/scheduleAppJs/schedule-app/src/assignments.txt"
 function addAssignment(type, subject, description, due) {
-    json_assignments = fs.readFileSync('E:/Coding Projects/desktopApps/electronApps/scheduleAppJs/schedule-app/src/assignments.txt');
+    json_assignments = fs.readFileSync(PATH_);
     assignments = JSON.parse(json_assignments);
     var d = new Date();
     time_uploaded = d.getHours().toString() + " " + d.getMinutes().toString() + " " + d.getSeconds().toString()  
     assignments.push({"type": type, "subject": subject, "description": description, "due": due, "time_uploaded": time_uploaded})
     assignments = JSON.stringify(assignments)
-    fs.writeFile('E:/Coding Projects/desktopApps/electronApps/scheduleAppJs/schedule-app/src/assignments.txt', assignments, function(err){});
+    fs.writeFile(PATH_, assignments, function(err){});
 }
 // addAssignment("assignment", "math", "ch4 ex5", "5th july")
 function readAssignments(){
-    json_assignments = fs.readFileSync('E:/Coding Projects/desktopApps/electronApps/scheduleAppJs/schedule-app/src/assignments.txt');
+    json_assignments = fs.readFileSync(PATH_);
     assignments = JSON.parse(json_assignments);
     return assignments
 }
 
 function deleteAssignment(time_uploaded){
     assignments = readAssignments()
-    index_of_item_to_remove = 0
-    is_only_one = false
-    if (assignments.length == 1) {
-        is_only_one = true
-    }
     assignments.forEach((assignment, index) => {
         if (assignment['time_uploaded'] == time_uploaded) {
-            index_of_item_to_remove = index
+            assignments.splice(index, 1)
         }
     })
-    assignments.splice(index_of_item_to_remove, 1)
-    if (is_only_one){
-        assignments = []
-        assignments = JSON.stringify(assignments)
-        fs.writeFile('E:/Coding Projects/desktopApps/electronApps/scheduleAppJs/schedule-app/src/assignments.txt', assignments, function(err){});    
-    
-    } else {
-        assignments = JSON.stringify(assignments)
-        fs.writeFile('E:/Coding Projects/desktopApps/electronApps/scheduleAppJs/schedule-app/src/assignments.txt', assignments, function(err){});    
-    }
+    assignments = JSON.stringify(assignments)
+    fs.writeFile(PATH_, assignments, function(err){}); 
 }
 
 module.exports = { addAssignment, readAssignments, deleteAssignment }
--- src/main.js
+++ src/main.js
@@ -1,26 +1,58 @@
 const { readAssignments, deleteAssignment, addAssignment } = require("./functions")
-const json_interaction = require("./functions")
-
+// const json_interaction = require("./functions")
 assignments = readAssignments()
 assignments_list = document.getElementById('main_assignments_content')
 exams_list = document.getElementById('main_exams_content')
 tba_a = ""
 tba_e = ""
-assignments.forEach((element, index) => {
+assignments.forEach(element=>{
+    date = element['due'].split('')
+    month = parseInt((date.splice(-2)).join(''), 10)
+    day = parseInt([date[0], [date[1]]].join(''), 10)
+    year = 2020
+    date = new Date(year, month, day)
+    element['due'] = day +"."+ month
+    element['date'] = month+(day/100)
+    element['day'] = day
+
+});
+assignments.sort((a,b) => { return a['date'] - b['date']})
+current_day = new Date().getDate();
+assignments.forEach(element => {
+    close = 0
+    if(element['day']-current_day<=2) {
+        close = 1
+    }
     if (element['type'] == 'assignment') {
         time_uploaded = element['time_uploaded']
-        tba_a = tba_a + `<tr> 
-        <td>${element['subject']}</td> 
-        <td>${element['description']}</td> <td>${element['due']}</td> 
-        <td><button class = "button is-success" onClick = "delete_('${time_uploaded}')">completed</button></td>
-        </tr>`
+        if(close == 1) {
+            tba_a = tba_a + `<tr class="has-background-danger"> 
+            <td>${element['subject']}</td> 
+            <td>${element['description']}</td> <td>${element['due']}</td> 
+            <td><button class = "button is-success" onClick = "delete_('${time_uploaded}')">completed</button></td>
+            </tr>`
+        } else {
+            tba_a = tba_a + `<tr> 
+            <td>${element['subject']}</td> 
+            <td>${element['description']}</td> <td>${element['due']}</td> 
+            <td><button class = "button is-success" onClick = "delete_('${time_uploaded}')">completed</button></td>
+            </tr>` 
+        }
     } else if (element['type'] == 'exam') {
         time_uploaded = element['time_uploaded']
-        tba_e = tba_e + `<tr> 
-        <td>${element['subject']}</td> 
-        <td>${element['description']}</td> <td>${element['due']}</td> 
-        <td><button class = "button is-success" onClick = "delete_('${time_uploaded}')">completed</button></td>
-        </tr>`
+        if(close == 1) {
+            tba_e = tba_e + `<tr class="has-background-danger"> 
+            <td>${element['subject']}</td> 
+            <td>${element['description']}</td> <td>${element['due']}</td> 
+            <td><button class = "button is-success" onClick = "delete_('${time_uploaded}')">completed</button></td>
+            </tr>`
+        } else {
+            tba_e = tba_e + `<tr> 
+            <td>${element['subject']}</td> 
+            <td>${element['description']}</td> <td>${element['due']}</td> 
+            <td><button class = "button is-success" onClick = "delete_('${time_uploaded}')">completed</button></td>
+            </tr>` 
+        }
     }
 });
 assignments_list.innerHTML = tba_a
